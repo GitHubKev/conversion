f41b65577c01a4414b8ff6e14d5700998c5c6493
Kevin <thekevinjung@gmail.com>
2021-04-25 23:02:34 -0400

    added kg to lb conversion

--- app.js
+++ app.js
@@ -7,31 +7,34 @@ const feetInput = document.querySelector(".feetInput")
 const inchInput = document.querySelector(".inchInput")
 const feetInchResult = document.querySelector(".feetInchResult")
 
-function feetToInch(){
-    feetInchResult.textContent = this.value*12 + " inch"
-}
-
-function addInch(){
-    feetInchResult.textContent = this.value;
-}
 feetInput.oninput = feetToInch;
-inchInput.oninput = addInch;
+inchInput.oninput = feetToInch;
 
-// user enters feet and inches
-// converts feet to inches
-// inches can be added to end result
-// results are updated 
+function feetToInch(){
+    if(inchInput.value > 0){
+        feetInchResult.textContent = feetInput.value + "ft " + inchInput.value + "in = " + (Number(feetInput.value*12) + Number(inchInput.value)) + " inch"
+    } else{
+        feetInchResult.textContent = feetInput.value + "ft = " + feetInput.value*12 + " inch"
+    }
+}
 
 // ml to oz conversion
 const mlInput = document.querySelector(".mlInput")
 const ozInput = document.querySelector(".ozInput")
 
 mlInput.oninput = mlToOz;
+ozInput.oninput = ozToMl;
 
-function mlToOz(){
-    result = this.value/29.574
-    x = result.toFixed(5) //max 5 digits
+function mlToOz(){ // converting ml to oz
+    result = this.value/29.57353
+    x = result.toFixed(4) //max 4 digits
     ozInput.value = x;
+}
+
+function ozToMl(){ //converting oz to ml
+    result = this.value * 29.57353
+    x = result.toFixed(4)
+    mlInput.value = x;
 }
 
 // cm to inch conversion
@@ -39,9 +42,33 @@ const cmInput = document.querySelector(".cmInput")
 const inchResult = document.querySelector(".inchResult")
 
 cmInput.oninput = cmToIn;
+inchResult.oninput = inToCm;
 
-function cmToIn(){
+function cmToIn(){ //converting cm to inches
     result = this.value/2.54
     x = result.toFixed(4)
     inchResult.value = x;
+}
+
+function inToCm(){ //converting inches to cm
+    result = this.value * 2.54
+    cmInput.value = result;
+}
+
+// kg to lb
+const kgInput = document.querySelector(".kgInput")
+const lbInput = document.querySelector(".lbInput")
+
+kgInput.oninput = kgToLb;
+lbInput.oninput = lbToKg;
+
+function kgToLb(){ //converting kilograms to pounds
+    result = this.value * 2.2046
+    lbInput.value = result;
+}
+
+function lbToKg(){ //converting pounds to kilograms
+    result = this.value / 2.2046
+    x = result.toFixed(4)
+    kgInput.value = x;
 }
